e23d7471ba94dade2c29d278793f8d2ea9e759c4
Michael B. E. Rickert <nobody@nowhere.noplace>
2010-06-19 07:42:39 -0700

    Initial skeleton commit

---
+++ SketchBook/Book.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SketchBook {
+	[Serializable] class PenStroke {
+		public int                   PenColor = unchecked((int)0xFF000000);
+		public float                 PenWidth = 1f;
+		public Pen CreatePen() { return new Pen(Color.FromArgb(PenColor)) { Width = PenWidth }; }
+		public SmoothingMode         SmoothingMode = SmoothingMode.AntiAlias;
+		public readonly List<PointF> Points        = new List<PointF>(); // relative to center of page
+		public void DrawTo( Graphics fx ) {
+			if ( Points.Count >= 2 ) {
+				var old_sm = fx.SmoothingMode;
+				fx.SmoothingMode = SmoothingMode;
+				using ( var pen = CreatePen() ) fx.DrawLines( pen, Points.ToArray() );
+				fx.SmoothingMode = old_sm;
+			}
+		}
+	}
+
+	[Serializable] class Page {
+		readonly List<PenStroke> Strokes = new List<PenStroke>();
+		[NonSerialized] Bitmap Cache;
+
+		public void AddStroke( PenStroke stroke ) {
+			Strokes.Add( stroke );
+			if ( Cache != null ) using ( var fx = Graphics.FromImage(Cache) ) {
+				fx.TranslateTransform( Cache.Width/2f, Cache.Height/2f );
+				stroke.DrawTo(fx);
+			}
+		}
+
+		public void DrawTo( Graphics rfx, int w, int h ) {
+			if (w%2==1) ++w;
+			if (h%2==1) ++h;
+
+			if ( Cache == null ) {
+				Cache = new Bitmap(w,h,PixelFormat.Format32bppArgb);
+				using ( var cfx = Graphics.FromImage(Cache) ) {
+					cfx.TranslateTransform( Cache.Width/2f, Cache.Height/2f );
+					foreach ( var stroke in Strokes ) stroke.DrawTo(cfx);
+				}
+			} else if ( Cache.Width < w || Cache.Height < h ) {
+				var oldw = Cache.Width;
+				var oldh = Cache.Height;
+				Cache.Dispose();
+				Cache = new Bitmap(Math.Max(oldw,w),Math.Max(oldh,h),PixelFormat.Format32bppArgb);
+				using ( var cfx = Graphics.FromImage(Cache) ) {
+					cfx.TranslateTransform( Cache.Width/2f, Cache.Height/2f );
+					foreach ( var stroke in Strokes ) stroke.DrawTo(cfx);
+				}
+			}
+
+			rfx.DrawImage( Cache, (w-Cache.Width)/2, (h-Cache.Height)/2, Cache.Width, Cache.Height );
+		}
+	}
+
+	[Serializable] class Book {
+		public readonly List<Page> Pages = new List<Page>() { new Page() };
+		int OpenPageIndex = 0;
+		public Page OpenPage { get {
+			return OpenPageIndex==-1 ? null : Pages[OpenPageIndex];
+		} set {
+			if ( value == null ) {
+				OpenPageIndex = -1;
+			} else {
+				var index = Pages.IndexOf(value);
+				if ( index == -1 ) throw new ArgumentException( "Page doesn't belong to this book" );
+				OpenPageIndex = index;
+			}
+		}}
+
+		private Book() {}
+
+		[NonSerialized] string Path;
+		public static Book CreateOrLoad( string path ) {
+			if ( File.Exists( path ) )
+			using ( var stream = File.Open(path,FileMode.Open,FileAccess.Read) )
+			{
+				var bf = new BinaryFormatter();
+				var book = (Book)bf.Deserialize(stream);
+				book.Path = path;
+				return book;
+			} else {
+				return new Book() { Path=path };
+			}
+		}
+
+		public void SaveToDisk() {
+			using ( var stream = File.Open(Path,FileMode.OpenOrCreate,FileAccess.Write) ) {
+				var bf = new BinaryFormatter();
+				bf.Serialize( stream, this );
+			}
+		}
+	}
+}
---
+++ SketchBook/PaperPage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SketchBook {
+	[System.ComponentModel.DesignerCategory("")]
+	class PaperForm : Form {
+		Book      Book = Book.CreateOrLoad( Path.Combine(Application.UserAppDataPath,"default.book") );
+		PenStroke CurrentStroke = null;
+		PointF ToCanvasCoordinate( PointF screencoord ) { return new PointF( screencoord.X - ClientSize.Width/2, screencoord.Y - ClientSize.Height/2 ); }
+
+		public PaperForm() {
+			BackColor       = Color.White;
+			DoubleBuffered  = true;
+			FormBorderStyle = FormBorderStyle.None;
+			StartPosition   = FormStartPosition.CenterScreen;
+			WindowState     = FormWindowState.Maximized;
+		}
+
+		protected override void OnPaint( PaintEventArgs e ) {
+			var fx = e.Graphics;
+
+			Book.OpenPage.DrawTo( fx, ClientSize.Width, ClientSize.Height );
+			fx.TranslateTransform( ClientSize.Width/2f, ClientSize.Height/2f );
+			if ( CurrentStroke != null ) CurrentStroke.DrawTo(fx);
+			base.OnPaint(e);
+		}
+
+		protected override void OnResize( EventArgs e ) {
+			Invalidate();
+			base.OnResize(e);
+		}
+
+		protected override void OnMouseDown( MouseEventArgs e ) {
+			switch ( e.Button ) {
+			case MouseButtons.Left:
+				CurrentStroke = new PenStroke() { Points = { ToCanvasCoordinate(e.Location) } };
+				break;
+			}
+			Invalidate();
+			base.OnMouseDown(e);
+		}
+
+		protected override void OnMouseMove( MouseEventArgs e ) {
+			if ( CurrentStroke != null ) CurrentStroke.Points.Add( ToCanvasCoordinate(e.Location) );
+			Invalidate();
+			base.OnMouseMove(e);
+		}
+
+		protected override void OnMouseUp( MouseEventArgs e ) {
+			if ( CurrentStroke != null ) CurrentStroke.Points.Add( ToCanvasCoordinate(e.Location) );
+			switch ( e.Button ) {
+			case MouseButtons.Left:
+				Book.OpenPage.AddStroke(CurrentStroke);
+				CurrentStroke = null;
+				Book.SaveToDisk();
+				break;
+			}
+			Invalidate();
+			base.OnMouseUp(e);
+		}
+
+		[STAThread] static void Main() {
+			Application.Run( new PaperForm() );
+		}
+	}
+}
